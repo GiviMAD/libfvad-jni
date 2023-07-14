5e928129d9a3c2017970e301fa88a2fbc713a29e
Miguel Álvarez <miguelwork92@gmail.com>
2023-07-14 16:19:39 +0200

    link win64 lib statically

--- build_win.ps1
+++ build_win.ps1
@@ -7,7 +7,7 @@ cd ..\..\..\..\
 
 g++ -c -std=c++11 -O3 -DNDEBUG -fPIC -I $env:JAVA_HOME\include -I $env:JAVA_HOME\include\win32 -I src\main\native\libfvad\include -I src\main\native src\main\native\io_github_givimad_libfvadjni_VoiceActivityDetector.cpp -o src\main\native\io_github_givimad_libfvadjni_VoiceActivityDetector.o
 
-g++ -shared -I src\main\native\libfvad\include -I src\main\native -o src\main\resources\win-amd64\libfvadjni.dll src\main\native\libfvad\src\CMakeFiles\fvad.dir\*.c.obj src\main\native\libfvad\src\CMakeFiles\fvad.dir\vad\*.c.obj src\main\native\libfvad\src\CMakeFiles\fvad.dir\signal_processing\*.c.obj src\main\native\libfvad\src\libfvad.a src\main\native\io_github_givimad_libfvadjni_VoiceActivityDetector.o
+g++ -shared -static -I src\main\native\libfvad\include -I src\main\native -o src\main\resources\win-amd64\libfvadjni.dll src\main\native\libfvad\src\CMakeFiles\fvad.dir\*.c.obj src\main\native\libfvad\src\CMakeFiles\fvad.dir\vad\*.c.obj src\main\native\libfvad\src\CMakeFiles\fvad.dir\signal_processing\*.c.obj src\main\native\libfvad\src\libfvad.a src\main\native\io_github_givimad_libfvadjni_VoiceActivityDetector.o
 
 if ($LastExitCode -ne 0) {
     Write-Error "Unable to build library"
